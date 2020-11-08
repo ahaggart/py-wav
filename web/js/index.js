f5d2e04ae88ad897794edf346967619edda5b73a
Alex Haggart <ahaggart@ucsd.edu>
2020-11-08 11:25:38 -0800

    start of html renderer

---
+++ web/js/index.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+
+$(document).ready(main);
+
+function main() {
+    $.get("song.json", createElements)
+}
+
+function createElements(data, status) {
+    const root_container = $("#root-container")
+    root_container.append(JSON.stringify(data))
+    createElement(root_container, data)
+}
+
+function createElement(parent, data) {
+    var child = $("<div></div>")
+    child.prop("id", data.uuid)
+    child.addClass("wav-source")
+    child.append(data.type)
+    parent.append(child)
+}
